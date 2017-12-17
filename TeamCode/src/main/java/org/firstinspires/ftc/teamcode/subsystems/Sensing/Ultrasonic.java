37d9c79241988cea60e66dfc4e0a79df554a6fc0
zdog1230 <zdog1230@yahoo.com>
2017-12-17 13:22:12 -0500

    Updates to Ultrasonic library test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Sensing/Ultrasonic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Sensing/Ultrasonic.java
@@ -1,17 +1,20 @@
 package org.firstinspires.ftc.teamcode.subsystems.Sensing;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Ultrasonic {
 
     HardwareMap hardwareMap;
+    LinearOpMode linearOpMode;
 
     private Wire ds;
     private int distance;
     private long pingTime;
 
-    public Ultrasonic(HardwareMap hardwareMapRef) {
+    public Ultrasonic(HardwareMap hardwareMapRef, LinearOpMode opMode) {
         hardwareMap = hardwareMapRef;
+        linearOpMode = opMode;
         ds = new Wire(hardwareMap,"ultrasonic",0xE0);
     }
 
@@ -28,7 +31,7 @@ public class Ultrasonic {
 
     public int getDistance() {
         distance = -1;
-        while (distance != -1) {
+        while ((distance != -1)&&(linearOpMode.opModeIsActive())) {
             //Write to Register to get Data from Ultrasonic
             if ((System.currentTimeMillis() - pingTime) > 100) {
                 ds.requestFrom(0, 2);
