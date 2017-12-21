197ef270d917a65565e021f2bb742846280a3851
emcloughlin <emcloughlin42@gmail.com>
2017-12-21 11:40:46 -0500

    Made UltraSonicTestNew A TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiagnosticOps/UltrasonicTestNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiagnosticOps/UltrasonicTestNew.java
@@ -3,20 +3,27 @@ package org.firstinspires.ftc.teamcode.DiagnosticOps;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import com.qualcomm.hardware.Maxbotix.I2CXL;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 /**
  * Created by Evan McLoughlin on 12/18/2017.
  */
-
+@TeleOp
 public class UltrasonicTestNew extends OpMode {
 
     I2CXL ultrasonic;
+    Telemetry telemetry;
+    HardwareMap hardwareMap;
 
     @Override
     public void init() {
 
         ultrasonic = hardwareMap.get(I2CXL.class, "ultrasonic");
+        ultrasonic.initialize();
+
 
     }
 
